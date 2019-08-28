17fa5a1c909490e9188a1b077998c38bdf0117d4
rank0619 <rank0619@ws-10083.netcracker.com>
2019-08-28 12:32:45 +0530

    First Commit

---
+++ src/main/java/com/medium/AwsUploadMedium1Application.java
@@ -0,0 +1,14 @@
+package com.medium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsUploadMedium1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsUploadMedium1Application.class, args);
+		System.out.println("Hello yaar");
+	}
+
+}
---
+++ src/main/java/com/medium/controller/BucketController.java
@@ -0,0 +1,35 @@
+package com.medium.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.medium.services.AmazonClient;
+
+@RestController
+@RequestMapping("/storage/")
+@Component
+public class BucketController {
+
+    private AmazonClient amazonClient;
+    
+    @Autowired
+    BucketController(AmazonClient amazonClient) {
+        this.amazonClient = amazonClient;
+    }
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
+        return this.amazonClient.uploadFile(file);
+    }
+
+    @DeleteMapping("/deleteFile")
+    public String deleteFile(@RequestPart(value = "url") String fileUrl) {
+        return this.amazonClient.deleteFileFromS3Bucket(fileUrl);
+    }
+}
